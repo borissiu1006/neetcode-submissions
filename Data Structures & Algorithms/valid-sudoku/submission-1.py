893da823f657aa2b836a19c91f40af9363b49a5e
SIU <borissiu1006@gmail.com>
2026-06-04 21:16:38 +1000

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,56 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check dublicates
+            rows = board
+            columns = [[] for _ in range(9)]
+            subboxes = [[] for _ in range(9)]
+
+            for num in rows:
+                checker = {}
+                for i in num:
+                    if i == '.':
+                        continue
+
+                    if i not in checker:
+                        checker[i] = 1
+                    else:
+                        return False
+
+            for col in range(9):
+                for row in range(9):
+                    val = board[row][col]
+                    if val == '.':
+                        continue
+
+                    if val not in columns[col]:
+                        columns[col].append(val)
+                    else:
+                        return False
+
+            box = 0
+            for row in range(9):
+                if 0 <= row <= 2:
+                    box = 0
+                elif 3 <= row <= 5:
+                    box = 3
+                elif 6 <= row <= 8:
+                    box = 6
+
+                for num in range(9):
+                    val = board[row][num]
+                    if val == '.':
+                        continue
+
+                    if 0 <= num <= 2:
+                        target = box
+                    elif 3 <= num <= 5:
+                        target = box + 1
+                    elif 6 <= num <= 8:
+                        target = box + 2
+
+                    if val not in subboxes[target]:
+                        subboxes[target].append(val)
+                    else:
+                        return False
+            
+            return True
