e3974e72fbe2dcc2dc72497b46dff3629076de22
SIU <borissiu1006@gmail.com>
2026-06-26 03:22:46 +1000

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+        self.Min = float('inf')
+        
+
+    def push(self, val: int) -> None:
+        if not self.s:
+            self.s.append(0)
+            self.Min = val
+        else:
+            self.s.append(val - self.Min)
+            self.Min = min(self.Min, val)
+        
+    def pop(self) -> None:
+        pop = self.s.pop()
+        if pop < 0:
+            self.Min = self.Min - pop # add back pop to restore as prev min
+
+        
+    def top(self) -> int:
+        top = self.s[-1]
+        if top > 0:
+            return top + self.Min
+        else:
+            return self.Min
+        
+
+    def getMin(self) -> int:
+        return self.Min
+        
